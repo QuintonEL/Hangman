5c49820cfbc9b1918a83d37f366876f66d105e30
Quinton LaBorde <quintonlaborde@gmail.com>
2025-01-31 17:49:45 -0700

    added skeleton components for the rest of the app functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
 import { useState } from "react";
 import words from "./wordList.json";
+import { HangmanDrawing } from "./HangmanDrawing";
+import { HangmanWord } from "./HangmanWord";
+import { Keyboard } from "./Keyboard";
 
 function App() {
   // update the app state with a random word each time the page is reloaded
   const [wordToGuess, setWordToGuess] = useState(() => {
     return words[Math.floor(Math.random() * words.length)]
-  });
+  })
 
   // initialize state with empty array of strings to keep track of guessed letters
-  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
 
-  return <div></div>
+  return (
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center"
+      }}
+    >
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>Lose Win</div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+    </div>
+  )
 }
 
 export default App
