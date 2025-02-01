9e752ba699a49b47bf4fc7f22bf55e58cc192bf5
Quinton LaBorde <quintonlaborde@gmail.com>
2025-01-31 17:37:05 -0700

    added states for random word to guess and the letter guessed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-
+import { useState } from "react";
+import words from "./wordList.json";
 
 function App() {
-  return <h1>Hi</h1>
+  // update the app state with a random word each time the page is reloaded
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)]
+  });
+
+  // initialize state with empty array of strings to keep track of guessed letters
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+  return <div></div>
 }
 
 export default App
